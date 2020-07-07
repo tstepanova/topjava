86d9d0f99d5680ac4c838d8dd9fd789a5a4b9fb7
tistepanova <tamara.stepanova85@gmail.com>
2020-07-07 16:59:30 +0300

    HW5 DataJpaMealRepository: one transaction

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
@@ -2,6 +2,7 @@ package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 
@@ -21,6 +22,7 @@ public class DataJpaMealRepository implements MealRepository {
         this.crudMealRepository = crudRepository;
     }
 
+    @Transactional
     @Override
     public Meal save(Meal meal, int userId) {
         if (!meal.isNew() && get(meal.getId(), userId) == null) {
