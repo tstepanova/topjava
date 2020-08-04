22f12152f412ba99d74b1eeb8cd986a720a38fd1
tistepanova <tamara.stepanova85@gmail.com>
2020-08-04 23:42:49 +0300

    HW8

---
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -0,0 +1,35 @@
+// $(document).ready(function () {
+$(function () {
+    makeEditable({
+            ajaxUrl: "meals/",
+            datatableApi: $("#datatable").DataTable({
+                "paging": false,
+                "info": true,
+                "columns": [
+                    {
+                        "data": "dateTime"
+                    },
+                    {
+                        "data": "description"
+                    },
+                    {
+                        "data": "calories"
+                    },
+                    {
+                        "defaultContent": "Edit",
+                        "orderable": false
+                    },
+                    {
+                        "defaultContent": "Delete",
+                        "orderable": false
+                    }
+                ],
+                "order": [
+                    [
+                        0,
+                        "desc"
+                    ]
+                ]
+            }),
+        });
+});
